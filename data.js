3b67489b1868c8beff02927f20f71c72249d5e07
Abhinaya <abhinayaraj999@gmail.com>
2024-05-01 00:56:40 +0530

    Creating my own resume data in JSON format

---
+++ data.js
@@ -0,0 +1,63 @@
+const resumeData = {
+      "name": "Abhinaya Raj Pasula",
+      "title": "Software Engineer",
+      "contact": {
+          "email": "abhinayaraj05@gmail.com",
+          "phone": "+1234567890",
+          "address": "123 AI Street, Hi-Tech City, Hyderabad, India"
+      },
+      "summary": "Experience of working majorly in Browser-side JavaScript environment",
+      "experience": [
+          {
+              "title": "Promo Admin Tool (PAT)",
+              "company": "GSPANN TECHNOLOGIES",
+              "location": "Hyderabad, India",
+              "dates": "2021 - 2021",
+              "description": "Created Promotions, Coupons, and User Bindings in the system."
+          },
+          {
+              "title": "DHL International Parcel logistics based Web Application",
+              "company": "IT SOURCE GLOBAL - CLIENT MINDTREE",
+              "location": "Bangalore, India",
+              "dates": "2020 - 2021",
+              "description": "Implemented different constraints and business logic for domestic (United Kingdom) and international shipments."
+          },
+          {
+              "title": "Client Portal Admin Dashboard",
+              "company": "XESOT TECHNOLOGIES",
+              "location": "Hyderabad, India",
+              "dates": "2017 - 2020",
+              "description": "Implemented CRUD operations for the client portal dashboard application."
+          }
+      ],
+      "education": [
+          {
+              "degree": "M.Tech",
+              "institution": "G NARAYANAMMA INSTITUTE OF TECHNOLOGY & SCIENCES, HYDERABAD",
+              "location": "JNTU, Hyderabad, India",
+              "dates": "2014 - 2017"
+          },
+          {
+              "degree": "B.Tech",
+              "institution": "SREE VISVESVARAYA INSTITUTE OF TECHNOLOGY & SCIENCES, MAHABUBNAGAR",
+              "location": "Mahabubnagar, Telangana, India",
+              "dates": "2010 - 2014"
+          }
+      ],
+      "skills": [
+          "React",
+          "React Router",
+          "Redux",
+          "Redux-Thunk",
+          "HTML",
+          "CSS",
+          "SASS",
+          "JavaScript",
+          "jQuery",
+          "Git",
+          "Bootstrap",
+          "Material-UI",
+          "WordPress",
+          "Webpack"
+      ]
+    }
